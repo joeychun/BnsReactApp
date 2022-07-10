8a20d28c15e3ffe5f6eb9a239b46651a405b3e50
joeychun <joey.s.chun@gmail.com>
2022-07-11 02:56:59 +0900

    created membersContent component. TODO: map members information to appropriate clusters of membersRow

---
+++ src/content/Member/membersContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MembersRow from './membersRow';
+
+const MembersContent = () => {
+    return (
+        <div>
+            <h1>Founder</h1>
+        </div>
+    )
+}
+
+export default MembersContent
