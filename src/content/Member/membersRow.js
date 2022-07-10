f9b921e35149e36ab5c5b569d4f2789e7b1c5026
joeychun <joey.s.chun@gmail.com>
2022-07-11 02:54:13 +0900

    created component that groups many memberContainers in one row

---
+++ src/content/Member/membersRow.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+import { Card, Avatar } from 'antd';
+
+import MemberContainer from './memberContainer';
+
+import Box from '@mui/material/Box';
+
+const { Meta } = Card;
+
+const MembersRow = ({ infoList }) => {
+    // This is how infoList will be formatted:
+    /* Example:
+    
+    [
+        {
+            name: "Junseo Lee",
+            picture: "junseo.png",
+            description: "Stuff"
+        },
+        {
+            name: "Andrew Shin",
+            picture: "andrew.png",
+            description: "Webmaster"
+        },
+    ]
+
+    */
+    const formattedList = [...infoList]
+    for (let i = 0; i < 3 - infoList.length; ++i) {
+        formattedList.push({ name: "", picture: "", description: "" })
+    }
+
+    const containers = formattedList.map((member) => {
+        return (
+            <Col className="gutter-row" span={8}><MemberContainer name={member.name} picture={member.picture} description={member.description} /></Col>
+        );
+    })
+
+    return (
+        <Row justify="center" gutter={24}>
+            {containers}
+        </Row>
+    );
+}
+
+export default MembersRow
