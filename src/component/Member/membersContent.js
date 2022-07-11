e7fa7617b4714443ad865fa77f27d8deac5abce1
joeychun <joey.s.chun@gmail.com>
2022-07-11 17:37:52 +0900

    changed members' order to be random

--- src/component/Member/membersContent.js
+++ src/component/Member/membersContent.js
@@ -3,14 +3,19 @@ import MembersRow from './membersRow';
 import AllMembers from '../../content/Members.json';
 
 const MembersContent = () => {
-    const SortedMembers = AllMembers.sort((a, b) => { // Alphabetically sorts AllMembers, by name
-        a.name.localeCompare(b.name)
-    })
+    // Alphabetically sorts AllMembers, by name
+    // const SortedMembers = AllMembers.sort((a, b) => a.name.localeCompare(b.name))
+
+    // Randomizes Members 
+    const SortedMembers = AllMembers
+        .map(value => ({ value, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ value }) => value)
     console.log(SortedMembers);
 
     let MembersInThree = [];
     for (let i = 0; i < SortedMembers.length; ++i) {
-        if (i % 3 == 0) {
+        if (i % 3 === 0) {
             MembersInThree.push([]);
         }
         MembersInThree[Math.floor(i / 3)].push(SortedMembers[i]);
