9b01509af891c50370d918fff8ebb67f5292fa81
joeychun <joey.s.chun@gmail.com>
2022-07-11 13:22:32 +0900

    changed name of dir containing react components to components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import Home from './content/home';
+import Home from './component/home';
 import { BrowserRouter, Routes, Route, Navigate, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <Routes>
-        <Route path='/' exact element={<Home show={true}/>} />
-      </Routes>
-    </div>
+      <div className="App">
+        <Routes>
+          <Route path='/' exact element={<Home show={true} />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
