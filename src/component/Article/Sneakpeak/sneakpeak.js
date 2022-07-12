11bc998f2a14fb6c15da98d34343f902a9b733ec
joeychun <joey.s.chun@gmail.com>
2022-07-12 19:21:03 +0900

    made Sneakpeak-related components, which have to do with article blurbs in /articles

---
+++ src/component/Article/Sneakpeak/sneakpeak.js
@@ -0,0 +1,42 @@
+import { Col, Row } from 'antd';
+import React from 'react';
+import { useNavigate } from 'react-router';
+import SearchAuthor from '../../../shared/Searcher/searchAuthor';
+
+import './styles.css';
+
+const stringExcerpt = (str) => {
+    const strArr = str.trim().split(/\s+/);
+
+    let returnStr = "";
+    for (let i = 0; i < 50; ++i) {
+        returnStr += " ";
+        returnStr += strArr[i];
+    }
+    returnStr += "...."
+    return returnStr;
+}
+
+const SneakPeak = ({ articleId, title, authorId, txt, date }) => {
+    const navigate = useNavigate();
+
+    const author = SearchAuthor(authorId);
+    const sneakText = stringExcerpt(txt);
+    return (
+        <div className="Sneakpeak">
+            <Row style={{ height: "100%" }} gutter={18}>
+                <Col style={{ width: "15%", borderRight: "1.5px solid #CDD1D4", paddingRight: 0, marginRight: 40 }} span={4}>
+                    <p className="Date">{date}</p>
+                </Col>
+                {/* <a href={`/articles/${articleId}`}> */}
+                <Col span={12}>
+                    <h2 onClick={() => navigate(`/articles/${articleId}`)}>{title}</h2>
+                    <p className="Blob">{sneakText}</p>
+                </Col>
+                {/* </a> */}
+            </Row>
+        </div>
+    )
+}
+
+export default SneakPeak
---
+++ src/component/Article/Sneakpeak/sneakpeaksBanner.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Banner from '../../../shared/Banner/banner';
+import EmptyBanner from '../../../shared/Banner/emptybanner';
+import SneakPeak from './sneakpeak';
+import { Flex } from 'rebass/styled-components';
+import { Footer } from 'antd/lib/layout/layout';
+import Articles from '../../../content/Articles.json'
+import TxtReader from '../../../shared/TxtReader/txtreader';
+import DateSorter from '../../../shared/Sorter/dateSorter';
+import { Col, Row } from 'antd';
+
+const content = {
+    heading: "Articles",
+    textBodies: [],
+    img: "",
+}
+
+const cssSettings = {
+    bgColor: "#364261",
+    maxWidth: "80%",
+}
+
+const SneakpeaksBanner = () => {
+    const allTextFiles = TxtReader();
+
+    const allSneaks = Articles.map(({ id, title, authorId, filename, date }) => {
+        let parsedFilename = filename.replace('.txt', ''); parsedFilename = parsedFilename.replace('.json', ''); // In case file format is added
+        return <SneakPeak key={id} articleId={id} title={title} authorId={authorId}
+            txt={allTextFiles[parsedFilename].text} date={date} />;
+    })
+
+    const sortedAllSneaks = allSneaks.sort((a, b) => {
+        return (
+            DateSorter(a.props.date, b.props.date) === 1 ? 1 : -1
+        );
+    })
+
+    return (
+        <div>
+            <Banner bannerId="" noDivider={true} bgColor={cssSettings.bgColor} heading={content.heading} textBodies={content.textBodies}
+                img={content.img} bgImg={content.bgImg} maxWidth={cssSettings.maxWidth} />
+            <div className="SneakpeaksBox">
+                {sortedAllSneaks}
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default SneakpeaksBanner;
