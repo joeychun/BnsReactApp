8a91bc0e5ed44b37343cab1f7d3de6e5c420271b
joeychun <joey.s.chun@gmail.com>
2022-07-14 01:59:34 +0900

    added more props regarding padding

--- src/shared/Banner/banner.js
+++ src/shared/Banner/banner.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import './banner.css'
 
-const Banner = ({ bannerId, noDivider, heading, textBodies, img, bgImg, bgColor, maxWidth, topPadding, bottomPadding }) => {
-    const body = textBodies.map((text, ind) => <div key={ind} className="Body"><p>{text}</p></div>);
+const Banner = ({ bannerId, noDivider, heading, textBodies, elementBody, img, bgImg,
+    bgColor, maxWidth, topPadding, bottomPadding, bodyLeftPadding, bodyTopPadding }) => {
+    /* Depending on what gets passed through textBodies or elementBody, the 'body' of the container changes. 
+    
+    textBodies format: List
+    [
+        "Sentence #1",
+        "Sentence #2",
+        "Sentence #3",
+        "Sentence #4"
+    ]
+
+    elementBody format: HTML Element / React Component
+    
+    */
+    let body;
+    if (textBodies !== [] && textBodies !== undefined) {
+        body = textBodies.map((text, ind) => <div key={ind}><p>{text}</p></div>);
+    } else if (elementBody !== null && elementBody !== undefined) {
+        body = elementBody;
+    } else {
+        body = ""
+    }
 
     const topPaddingValue = topPadding === undefined ?
         90 : topPadding
     const bottomPaddingValue = bottomPadding === undefined ?
         60 : bottomPadding
+    const bodyLeftPaddingValue = bodyLeftPadding === undefined ?
+        20 : bodyLeftPadding
+    const bodyTopPaddingValue = bodyTopPadding === undefined ?
+        0 : bodyTopPadding
 
     return (
         <div id={bannerId === "" ? "section" : `section ${bannerId}`}
@@ -15,8 +40,7 @@ const Banner = ({ bannerId, noDivider, heading, textBodies, img, bgImg, bgColor,
             <div className="Container">
                 <div style={{ maxWidth: maxWidth }} className="InfoCol">
                     <div className="Heading"><h1 style={{ borderBottom: noDivider ? "" : "1px solid #CDD1D4" }}>{heading}</h1></div>
-                    <div>{body}</div>
-
+                    <div className="Body" style={{ paddingTop: bodyTopPaddingValue, paddingLeft: bodyLeftPaddingValue }}>{body}</div>
                 </div>
                 <div style={{ maxWidth: maxWidth }} className="InfoCol">
                     {img !== "" &&
