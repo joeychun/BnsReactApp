1971acc60def8c70b5931a958f03e35012212313
joeychun <joey.s.chun@gmail.com>
2022-07-14 01:59:59 +0900

    added a mailto button

--- src/component/ContactBanners/contactbanner.js
+++ src/component/ContactBanners/contactbanner.js
@@ -1,22 +1,39 @@
 import React from 'react';
 import Banner from '../../shared/Banner/banner';
+import { Button, Icon } from 'semantic-ui-react'
+import './contactbanner.css';
+
+const ElementBody = () => {
+    return (
+        <Button animated="vertical" color="blue" className="ContactButton"
+            onClick={() => window.open("mailto:seongyoon.kim24@stu.siskorea.org")}
+        >
+            <Button.Content visible><p>Email Us!</p></Button.Content>
+            <Button.Content hidden>
+                <Icon name='mail' size="large" />
+            </Button.Content>
+        </Button>
+    );
+}
 
 const content = {
     heading: "Contact",
-    textBodies: [],
     img: "",
 }
 
 const cssSettings = {
     bgColor: "#364261",
     maxWidth: "80%",
+    bodyTopPadding: 15,
+    bodyLeftPadding: 80
 }
 
 const ContactBanner = () => {
     return (
         <div>
-            <Banner bannerId="" noDivider={true} bgColor={cssSettings.bgColor} heading={content.heading} textBodies={content.textBodies}
-                img={content.img} bgImg={content.bgImg} maxWidth={cssSettings.maxWidth} />
+            <Banner bannerId="" noDivider={true} bgColor={cssSettings.bgColor} heading={content.heading} elementBody={<ElementBody />}
+                img={content.img} bgImg={content.bgImg} maxWidth={cssSettings.maxWidth}
+                bodyTopPadding={cssSettings.bodyTopPadding} bodyLeftPadding={cssSettings.bodyLeftPadding} />
         </div>
     )
 }
