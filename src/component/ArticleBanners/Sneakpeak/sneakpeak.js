3282a8553e835d5e72748b54372e4ccc8a90d1fd
joeychun <joey.s.chun@gmail.com>
2022-07-13 09:55:47 +0900

    chagned back to navigate, for now

--- src/component/ArticleBanners/Sneakpeak/sneakpeak.js
+++ src/component/ArticleBanners/Sneakpeak/sneakpeak.js
@@ -1,6 +1,6 @@
 import { Col, Row } from 'antd';
 import React from 'react';
-// import { useNavigate } from 'react-router';
+import { useNavigate } from 'react-router';
 
 import './styles.css';
 
@@ -17,7 +17,7 @@ const stringExcerpt = (str) => {
 }
 
 const SneakPeak = ({ articleId, title, txt, date }) => {
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
 
     const sneakText = stringExcerpt(txt);
     return (
@@ -27,9 +27,10 @@ const SneakPeak = ({ articleId, title, txt, date }) => {
                     <p className="Date">{date}</p>
                 </Col>
                 <Col span={12}>
-                    <a href={`/articles/${articleId}`}>
+                    {/* <a href={`/articles/${articleId}`}>
                         <h2>{title}</h2>
-                    </a>
+                    </a> */}
+                    <h2 onClick={() => navigate(`/articles/${articleId}`)}>{title}</h2>
                     <p className="Blob">{sneakText}</p>
                 </Col>
             </Row>
