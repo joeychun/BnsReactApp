4e861bcc6ca3a6fe20369de2937fd6d6ba5aab09
joeychun <joey.s.chun@gmail.com>
2022-07-13 22:12:32 +0900

    minor changes

--- src/component/ArticleBanners/Article/oneArticle.js
+++ src/component/ArticleBanners/Article/oneArticle.js
@@ -3,8 +3,9 @@ import SearchArticle from '../../../shared/Searcher/searchArticle';
 import SearchAuthor from '../../../shared/Searcher/searchAuthor';
 import TxtReader from '../../../shared/TxtReader/txtreader';
 import Box from '@mui/material/Box';
-import './oneArticle.css';
 import { Row } from 'antd';
+import './oneArticle.css';
+
 
 const OneArticle = ({ id }) => {
     const { title, authorId, filename, date } = SearchArticle(id);
@@ -17,9 +18,9 @@ const OneArticle = ({ id }) => {
     // The process below adds in newlines by inserting <br>
     const textArray = text.split(/\n/);
     console.log(textArray);
-    const textParagraphs = textArray.map((paragraph) => {
+    const textParagraphs = textArray.map((paragraph, ind) => {
         return (
-            <div className="paragraph">
+            <div key={ind} className="paragraph">
                 <p>{paragraph}</p>
             </div>
         );
