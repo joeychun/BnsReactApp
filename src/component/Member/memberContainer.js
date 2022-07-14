add236af3bc3751134b438417a855e96af410ae1
joeychun <joey.s.chun@gmail.com>
2022-07-15 04:14:13 +0900

    added <a href> to profile link page

--- src/component/Member/memberContainer.js
+++ src/component/Member/memberContainer.js
@@ -1,20 +1,22 @@
 import { Card } from 'antd';
 import React from 'react';
 import EmptyContainer from './emptyContainer';
-import './styles.css'
+import './styles.css';
 
-const MemberContainer = ({ name, picture, description }) => {
+const MemberContainer = ({ id, name, picture, description }) => {
     const srcLink = `img/Members/${picture}`;
     return (
         <div>
             { (name || picture || description) ?
-                <Card
-                    flexDirection='row'
-                    hoverable
-                    cover={<img alt={name} src={srcLink} />}
-                >
-                    <Card.Meta title={name} description={description} />
-                </Card>
+                <a href={`/profile/${id}`}>
+                    <Card
+                        flexDirection='row'
+                        hoverable
+                        cover={<img alt={name} src={srcLink} />}
+                    >
+                        <Card.Meta title={name} description={description} />
+                    </Card>
+                </a>
                 :
                 <EmptyContainer />
             }
