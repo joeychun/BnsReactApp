975664fec3884f92b105770e02b3d163e5e201d2
joeychun <joey.s.chun@gmail.com>
2022-07-14 14:53:34 +0900

    allowed multiple (list of) authors to be assigned for one article

--- src/component/ArticleBanners/Article/oneArticle.js
+++ src/component/ArticleBanners/Article/oneArticle.js
@@ -6,10 +6,55 @@ import Box from '@mui/material/Box';
 import { Row } from 'antd';
 import './oneArticle.css';
 
+const AuthorsElement = (authorId) => {
+    /* authorId format:
+
+    If the article is single-author: 
+    authorId is a Number, the author's id 
+    (ex: 10)
+
+    If the article is multi-author:
+    authorId is a list of Numbers, the authors' ids
+    (ex: [3, 15, 9])
+    */
+    let authors;
+    if (typeof (authorId) === "number") {
+        authors = [<b>{SearchAuthor(authorId).name}</b>];
+    } else if (authorId.length === 2) {
+        authors = authorId.map((id, ind) => {
+            return (
+                <div style={{ display: "inline" }}>
+                    <b>{SearchAuthor(id).name}</b>
+                    {ind === 0 &&
+                        " and "
+                    }
+                </div>
+            );
+        })
+    }
+    else {
+        authors = authorId.map((id, ind) => {
+            return (
+                <div style={{ display: "inline" }}>
+                    <b>{SearchAuthor(id).name}</b>
+                    {ind !== authorId.length - 1 &&
+                        ", "
+                    }
+                    {ind === authorId.length - 2 &&
+                        "and "
+                    }
+                </div>
+            );
+        })
+    }
+
+    return authors;
+}
 
 const OneArticle = ({ id }) => {
     const { title, authorId, filename, date } = SearchArticle(id);
-    const author = SearchAuthor(authorId);
+
+    const authorStr = AuthorsElement(authorId);
 
     const parsedFilename = filename.replace('.txt', '').replace('.json', '');
     const text = TxtReader()[parsedFilename].text;
@@ -17,7 +62,6 @@ const OneArticle = ({ id }) => {
     // As 'text' is derived from a JSON file, there are no newlines.
     // The process below adds in newlines by inserting <br>
     const textArray = text.split(/\n/).filter(t => t);
-    console.log(textArray);
     const textParagraphs = textArray.map((paragraph, ind) => {
         return (
             <div key={ind} className="paragraph">
@@ -32,7 +76,7 @@ const OneArticle = ({ id }) => {
                 <h1 className="title">{title}</h1>
             </Row>
             <Row>
-                <h5 className="author">By <b>{author.name}</b></h5>
+                <h5 className="author">By {authorStr}</h5>
             </Row>
             <div className="paragraphs">
                 {textParagraphs}
