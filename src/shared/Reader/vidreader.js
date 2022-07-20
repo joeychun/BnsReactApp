e2fbff9da89c0d81d1423447fe814be588a33ddc
joeychun <joey.s.chun@gmail.com>
2022-07-20 16:26:51 +0900

    added another reader for vids

---
+++ src/shared/Reader/vidreader.js
@@ -0,0 +1,23 @@
+const TxtReader = (videos) => {
+    let context;
+    if (videos === undefined) {
+        context = require.context('../../content/vids', true, /.json$/).keys();
+    } else {
+        context = videos;
+    }
+
+    const all = {};
+    context.forEach((key) => {
+        const fileName = key.replace('./', '');
+        const resource = require(`../../content/vids/${fileName}`);
+        const namespace = fileName.replace('.json', '');
+        all[namespace] = JSON.parse(JSON.stringify(resource));
+
+    });
+    return all;
+}
+
+export default TxtReader;
+
+
+
