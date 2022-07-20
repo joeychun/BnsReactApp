56b197645b9552a6c5d4f8dd94075eed209782eb
joeychun <joey.s.chun@gmail.com>
2022-07-21 05:14:16 +0900

    made profile show both author's articles and vids

--- src/component/MemberProfile/profileContributions.js
+++ src/component/MemberProfile/profileContributions.js
@@ -1,28 +1,48 @@
 import React from 'react';
 import SneakpeaksBanner from '../../shared/Sneakpeak/sneakpeaksBanner';
 import FilterArticle from '../../shared/Filter/filterArticle';
+import FilterVideos from '../../shared/Filter/filterVideo';
 import TxtReader from '../../shared/Reader/txtreader';
+import VidReader from '../../shared/Reader/vidreader';
+import YoutubeThumbnail from '../../shared/YoutubeEmbed/thumbnail';
+import ThumbnailWrapper from '../../shared/ThumbnailWrapper/thumbnailWrapper';
 
 const stringExcerpt = (str) => {
     return str.split(" ").slice(0, 50).join(" ") + "....";
 }
 
+const linkFunc = (articleId) => {
+    return `/articles/${articleId}`;
+}
+
 const ProfileContributions = ({ id }) => {
     const articles = FilterArticle(parseInt(id));
+    const videos = FilterVideos(parseInt(id));
+    const sources = articles.concat(videos);
 
     const articlePaths = articles.map((element) => `./${element.filename}`);
+    const videoPaths = videos.map((element) => `./${element.filename}`);
 
     const allTextFiles = TxtReader(articlePaths);
+    const allVidFiles = VidReader(videoPaths);
+    const allFiles = { ...allTextFiles, ...allVidFiles };
     const contentFunc = (unprocessedContent) => {
-        return <p>{stringExcerpt(unprocessedContent.text)}</p>;
+        // if (unprocessedContent.type === "article") { <-- TODO : fix
+        if (unprocessedContent.videoId === undefined) {
+            return <p>{stringExcerpt(unprocessedContent.text)}</p>;
+            // } else if (unprocessedContent.type === "video") {
+        } else {
+            const thumbnail = <YoutubeThumbnail videoId={unprocessedContent.videoId} />
+            return <ThumbnailWrapper thumbnail={thumbnail} description={unprocessedContent.text} />
+        }
     }
 
     return (
         <div>
-            {articles.length !== 0 &&
+            {sources.length !== 0 &&
                 <div className="ProfileSneaks">
-                    <SneakpeaksBanner contentJsons={articles} allFiles={allTextFiles}
-                        contentFunc={(c) => contentFunc(c)} includeMargin={false} />
+                    <SneakpeaksBanner contentJsons={sources} allFiles={allFiles}
+                        contentFunc={(c) => contentFunc(c)} linkFunc={(sourceId) => linkFunc(sourceId)} includeMargin={false} />
                 </div>
 
             }
