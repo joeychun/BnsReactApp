5a20b196af40bf4560fbfc7b3c9cf75c11679f59
joeychun <joey.s.chun@gmail.com>
2022-07-22 03:52:20 +0900

    made single-video component

---
+++ src/component/VideoBanners/Video/oneVideo.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Searcher from '../../../shared/Searcher/searcher';
+import Reader from '../../../shared/Reader/reader';
+import AuthorsSection from '../../../shared/AuthorsSection/authorsSection';
+import YoutubeEmbed from '../../../shared/YoutubeEmbed/youtubeEmbed';
+import Box from '@mui/material/Box';
+import { Row } from 'antd';
+import './oneVideo.css';
+
+
+const OneVideo = ({ id }) => {
+    const { title, authorId, filename, date } = Searcher("video", id);
+
+    const authorStr = AuthorsSection(authorId);
+
+    const parsedFilename = filename.replace('.txt', '').replace('.json', '');
+    const { text, videoId } = Reader({ video: null })[parsedFilename];
+
+    // As 'text' is derived from a JSON file, there are no newlines.
+    // The process below adds in newlines by inserting <br>
+    const textArray = text.split(/\n/).filter(t => t);
+    const textParagraphs = textArray.map((paragraph, ind) => {
+        return (
+            <div key={ind} className="paragraph">
+                <p>{paragraph}</p>
+            </div>
+        );
+    })
+
+    return (
+        <Box className="Video">
+            <Row>
+                <h1 className="title">{title}</h1>
+            </Row>
+            <Row>
+                <h5 className="author">By {authorStr}</h5>
+            </Row>
+            <YoutubeEmbed videoId={videoId} />
+            <div className="paragraphs">
+                {textParagraphs}
+            </div>
+            <Row>
+                <h5 className="date">Posted on <b>{date}</b></h5>
+            </Row>
+        </Box>
+    )
+}
+
+export default OneVideo;
