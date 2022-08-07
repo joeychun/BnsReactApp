b0b119a2bdcc0ecfef100d1837e91e175eec305d
joeychun <joey.s.chun@gmail.com>
2022-08-08 02:31:34 +0900

    added FooterBar Component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Contact from './pages/contact';
 import Profile from './pages/profile';
 
 import Menu from './component/Header/menu';
+import FooterBar from './component/Footer/footerbar';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
@@ -17,16 +18,18 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Menu />
-        <Routes>
-          <Route path='/' exact element={<Home />} />
-          <Route path='/articles/:id' exact element={<Article />} />
-          <Route path='/articles' exact element={<Articles />} />
-          <Route path='/videos/:id' exact element={<Video />} />
-          <Route path='/videos' exact element={<Videos />} />
-          <Route path='/contact' exact element={<Contact />} />
-          <Route path='/profile/:id' exact element={<Profile />} />
-        </Routes>
-        {/* <FooterBar /> */}
+        <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
+          <Routes>
+            <Route path='/' exact element={<Home />} />
+            <Route path='/articles/:id' exact element={<Article />} />
+            <Route path='/articles' exact element={<Articles />} />
+            <Route path='/videos/:id' exact element={<Video />} />
+            <Route path='/videos' exact element={<Videos />} />
+            <Route path='/contact' exact element={<Contact />} />
+            <Route path='/profile/:id' exact element={<Profile />} />
+          </Routes>
+        </div>
+        <FooterBar />
       </div>
     </BrowserRouter>
   );
