eedbeb3540bb36a6d5a1581435f9002e2d616743
joeychun <joey.s.chun@gmail.com>
2022-08-08 02:33:18 +0900

    made footer component

--- src/component/Footer/footerbar.js
+++ src/component/Footer/footerbar.js
@@ -1,39 +1,41 @@
 import React from 'react';
-import { Row } from 'antd';
-import { NavLink } from 'react-router-dom';
-import SocialLink from './social';
+import { Row, Col } from 'antd';
+import { Link } from 'react-router-dom';
+import { Icon } from 'semantic-ui-react';
 import './styles.css';
+
 
 const FooterBar = () => {
     return (
-        <Row
-            type="flex"
-            justify="space-between"
-            align="middle"
-            style={{ paddingTop: "3rem", borderTop: "1px solid #CDD1D4" }}
-        >
-            <NavLink className="Links" to="/">
-                <div className="Icon">
-                    <div className="ImageContainer">
-                        <img src={`/img/bnslogo.jpeg`} alt="bnslogo.jpeg" width="40%" height="100%" />
-                    </div>
+        <Col className="FooterStyle">
+            <Row className="Footer"
+                type="flex"
+                justify="space-between"
+                align="middle"
+                style={{ paddingTop: "3rem", paddingBottom: "3rem", rowGap: "15px", borderTop: "1px solid #CDD1D4" }}
+            >
+                <div className="ImageContainer">
+                    <Link className="Links" to="/">
+                        <img className="Image" src={`/img/bnslogo.jpeg`} alt="bnslogo.jpeg" width="60%" height="100%" />
+                    </Link>
                 </div>
-            </NavLink>
-            <div className="LinkContainer">
-                <SocialLink
-                    href="https://youtube.com"
-                    src="youtube.jpeg"
-                />
-                {/* <SocialLink
-                    href="https://twitter.com/friendliai"
-                    src="twitter.svg"
-                />
-                <SocialLink
-                    href="https://medium.com/@friendliai/"
-                    src="medium.svg"
-                /> */}
-            </div>
-        </Row>
+
+                <div className="LinkContainer">
+                    <a href="https://www.youtube.com/channel/UCFQ8WlLN-ye9VLUEsDJOzjA" rel="noreferrer" target="_blank">
+                        <Icon link color="red" name="youtube" size="big" />
+                    </a>
+                    <a href="mailto:seongyoon.kim24@stu.siskorea.org" rel="noreferrer" target="_blank">
+                        <Icon link color="blue" name="mail" size="big" />
+                    </a>
+                    <a href="https://www.instagram.com/behavioral_neuroscience/" rel="noreferrer" target="_blank">
+                        <Icon link color="pink" name="instagram" size="big" />
+                    </a>
+                </div>
+            </Row>
+            <Row>
+                <p className="MadeBy">Website made by Joey C and Andrew S</p>
+            </Row>
+        </Col >
     )
 }
 
